abc5a6026a1a51a7b7466d9ab570919177cde284
Your Name <you@example.com>
2022-12-05 20:17:37 +0600

    2

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+console.log("sks");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text className=" bg-red-10">Sk.Sulaimans</Text>
+      <Text className="text-blue-100">Sk.Sulaimans</Text>
     </View>
   );
 }
